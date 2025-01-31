64ffe906976ca0765d2ae0ad3aed2bafc153b054
Fabian Schmauder <fabianschmauder@gmail.com>
2025-01-31 15:24:06 +0100

    Init project

---
+++ src/main/kotlin/de/fabianschmauder/incode/technologies/exercise/IncodeTechnologiesExerciseApplication.kt
@@ -0,0 +1,11 @@
+package de.fabianschmauder.incode.technologies.exercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IncodeTechnologiesExerciseApplication
+
+fun main(args: Array<String>) {
+    runApplication<IncodeTechnologiesExerciseApplication>(*args)
+}
---
+++ src/test/kotlin/de/fabianschmauder/incode/technologies/exercise/IncodeTechnologiesExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.fabianschmauder.incode.technologies.exercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IncodeTechnologiesExerciseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
